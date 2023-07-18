d7762ea0ee1903240bd462b0d5279d43116bf354
Mohamed Ayman <mayman@atlassian.com>
2023-07-18 10:58:52 +0200

    initial commit

---
+++ src/main/java/MyDerivedClass.java
@@ -0,0 +1,8 @@
+// MyDerivedClass.java
+package com.mycompany.myapp;
+
+public class MyDerivedClass extends MyClass {
+    public void printMessage() {
+        System.out.println("Hello from MyDerivedClass");
+    }
+}
